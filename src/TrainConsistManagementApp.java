824d41c8fe9c82dd68112ab218d20181004d3d6e
harsha007-in <harshavarthansb007@gmail.com>
2026-03-30 08:55:34 +0530

    UC1: Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        // Header
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("===================================");
+
+        // Initialize Train Consist
+        List<String> trainConsist = new ArrayList<>();
+
+        // Output Messages
+        System.out.println("\nTrain initialized successfully...");
+        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("Current Train Consist : " + trainConsist);
+
+        System.out.println("\nSystem ready for operations...");
+    }
 }
